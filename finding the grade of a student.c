923552edf0b059aba62164901bc3df04fef119b0
papireddy903 <97383201+papireddy903@users.noreply.github.com>
2022-01-11 19:37:28 +0530

    Add files via upload

---
+++ finding the grade of a student.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+
+    int tel,hin,eng,mat,sci,soc;
+    float avg;
+    printf("enter the marks of all subjects:");
+    scanf("%d %d %d %d %d %d",&tel ,&hin ,&eng ,&mat ,&sci,&soc);
+    avg=(float)((tel+hin+eng+mat+sci+soc)/6);
+    printf("\nyour avg score is %.2f:\n",avg);
+
+    if (avg>=91)
+        printf("you got A+ grade!");
+    else if (avg>=71 && avg<=90)
+        printf("you got A grade!");
+    else if (avg>=61 && avg<=70)
+        printf("you got B+ grade!");
+    else if (avg>=51 && avg<=60)
+        printf("you got B grade!");
+    else if (avg>=41 && avg<=50)
+        printf("you got C grade!");
+    else if (avg<=40)
+        printf("Sorry,you are failed!");
+
+
+
+}
