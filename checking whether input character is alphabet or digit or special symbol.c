96f1dc983b56b49e76016f41f5f464ddbc96e8ab
papireddy903 <97383201+papireddy903@users.noreply.github.com>
2022-01-11 20:06:13 +0530

    Add files via upload

---
+++ checking whether input character is alphabet or digit or special symbol.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("enter any character:");
+    scanf("%c",&ch);
+    if ((ch>='a'&& ch<='z')||(ch>='A'&& ch<='Z'))
+        printf("\n%c is an alphabet",ch);
+    else if (ch>='0' && ch<'10')
+        printf("\n%c is a digit!\n",ch);
+    else
+        printf("%c is a special symbol!",ch);
+    return 0;
+}
+
